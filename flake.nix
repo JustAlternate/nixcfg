65329e8c0d5fba1453d592493d58d6fa7f684319
JustAlternate <loicw@justalternate.com>
2024-08-03 00:54:24 +0200

    foldering

--- flake.nix
+++ flake.nix
@@ -2,10 +2,16 @@
   description = "Nixos config flake";
 
   inputs = {
+
+    # nixpkgs
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    unstable.url = "nixpkgs/nixos-unstable";
+
+    # home-manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    lobster.url = "github:justchokingaround/lobster";
+
+    # other urls
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
         url = "github:hyprwm/hyprland-plugins";
@@ -14,27 +20,29 @@
     nixvim = {
       url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
+
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, unstable, home-manager, ... } @ inputs:
     let
-      lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
 
     nixosConfigurations = {
-      nixos = lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-	modules = [ ./configuration.nix ];
+	modules = [ 
+	  ./nixos/configuration.nix
+	];
       };
     };
     homeConfigurations = {
       justalternate = home-manager.lib.homeManagerConfiguration {
 	inherit pkgs;
 	modules = [
-	  ./home.nix
+	  ./home-manager/home.nix
 	];
         extraSpecialArgs = {
           inherit inputs;
