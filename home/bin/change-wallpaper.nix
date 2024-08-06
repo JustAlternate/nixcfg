1d7c7a3b89df0fc90f2b68869cafb04aef48cd74
JustAlternate <loicw@justalternate.com>
2024-08-06 19:51:02 -0400

    fix(laptop): pywalfox auto reload

--- home/bin/change-wallpaper.nix
+++ home/bin/change-wallpaper.nix
@@ -8,7 +8,8 @@ pkgs.writeShellScriptBin "change-wallpaper"
     sleep 0.01
     eww reload &
     /./home/justalternate/.config/dotfiles/home/rice/pywal/pywal-discord/pywal-discord &
-    sleep 0.5
+    pywalfox update &
+    sleep 0.25
     cp ~/.cache/wal/cava_conf ~/.config/cava/config &
     [[ $(pidof cava) != "" ]] && pkill -USR1 cava &
   ''
