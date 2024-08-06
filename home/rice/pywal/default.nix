3cc2756f7cb723679a1abd6d386dc310a429f1ce
JustAlternate <lololollololol890@gmail.com>
2024-08-06 19:51:02 -0400

    fix(laptop): pywalfox auto reload

--- home/rice/pywal/default.nix
+++ home/rice/pywal/default.nix
@@ -1,22 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 let
-  pywalfox = pkgs.python39.pkgs.buildPythonPackage rec {
-    pname = "pywalfox";
-    version = "2.8.0rc1";
-    doCheck = false;
-    src = pkgs.fetchurl {
-      url = "https://test-files.pythonhosted.org/packages/89/a1/8e011e2d325de8e987f7c0a67222448b252fc894634bfa0d3b3728ec6dbf/pywalfox-2.8.0rc1.tar.gz";
-      sha256 = "89e0d7a441eb600933440c713cddbfaecda236bde7f3f655db0ec20b0ae12845"; # Replace with the correct SHA256 hash
-    };
-  };
-in {
-  home.packages = with pkgs;
-    [
-      (python39.withPackages (ps:
-        with ps; [
-          pywalfox
-        ]))
+  pywalfox-wrapper = pkgs.writeShellScriptBin "pywalfox-wrapper" ''
+    ${pkgs.pywalfox-native}/bin/pywalfox start
+  '';
+in
+{
+  home.packages = with pkgs; [
+    pywal
+    pywalfox-native
+  ];
 
-      pywal
-    ];
+  home.file.".mozilla/native-messaging-hosts/pywalfox.json".text = lib.replaceStrings [ "<path>" ] [
+    "${pywalfox-wrapper}/bin/pywalfox-wrapper"
+  ] (lib.readFile "${pkgs.pywalfox-native}/lib/python3.11/site-packages/pywalfox/assets/manifest.json");
 }
