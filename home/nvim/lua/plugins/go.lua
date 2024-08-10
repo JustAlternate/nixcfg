726d66de4fb71aff0075fe03d7e8a321f3787fe5
JustAlternate <loicw@justalternate.com>
2024-08-10 13:57:18 -0400

    go nvim

---
+++ home/nvim/lua/plugins/go.lua
@@ -0,0 +1,17 @@
+return {
+	"ray-x/go.nvim",
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("go").setup({})
+
+		require("lspconfig").gopls.setup({
+			-- your gopls setup
+		})
+	end,
+	event = { "CmdlineEnter" },
+	ft = { "go", "gomod" },
+	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}
