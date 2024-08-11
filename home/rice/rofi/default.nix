b408a819660cb31c0185c38270f0d766b2484134
JustAlternate <lololollololol890@gmail.com>
2024-08-11 14:04:30 -0400

    adding default rofi config

--- home/rice/rofi/default.nix
+++ home/rice/rofi/default.nix
@@ -4,6 +4,7 @@ _: {
       "rofi/bottom_large.rasi".source = ./bottom_large.rasi;
       "rofi/wall_select.rasi".source = ./wall_select.rasi;
       "rofi/bemoji.rasi".source = ./bemoji.rasi;
+      "rofi/config.rasi".source = ./config.rasi;
     };
   };
 }
