5e861454e1e2e52690bfdcaeb0323110f7b29c13
JustAlternate <loicw@justalternate.com>
2024-08-14 21:49:38 -0400

    feat(adding dev to home)

--- home/dev/R/default.nix
+++ home/dev/R/default.nix
@@ -1,17 +1,17 @@
 { pkgs
 , ...
 }:
-let
-  list-packages = with rPackages; [
-    ggplot2
-    dplyr
-    xts
-  ];
-  r-with-packages = rWrapper.override { packages = list-packages; };
-in
 {
   home = {
     packages = with pkgs;
+      let
+        list-packages = with rPackages; [
+          ggplot2
+          dplyr
+          xts
+        ];
+        r-with-packages = rWrapper.override { packages = list-packages; };
+      in
       [
         r-with-packages
       ];
