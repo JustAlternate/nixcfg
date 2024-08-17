b44bad1f58422f78bb8ae6bf56b4402e324270c6
JustAlternate <loicw@justalternate.com>
2024-08-16 20:37:32 -0400

    feat(remove llm)

--- home/nvim/lua/plugins/llm.lua
+++ home/nvim/lua/plugins/llm.lua
@@ -1,21 +1,36 @@
 return {
-	"huggingface/llm.nvim",
+	-- Minimal configuration
+	"David-Kunz/gen.nvim",
+	cmd = "Gen",
 	opts = {
-		model = "codellama:7b", -- the model ID, behavior depends on backend
-		backend = "ollama", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
-		url = "http://localhost:11434/api/generate", -- the http url of the backend
-		-- parameters that are added to the request body, values are arbitrary, you can set any field:value pair here it will be passed as is to the backend
-		request_body = {
-			parameters = {
-				temperature = 0.2,
-				top_p = 0.95,
-			},
-		},
-		context_window = 400, -- send less context for faster reply
-		enable_suggestions_on_startup = true,
-		enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
-		lsp = {
-			bin_path = "/nix/store/ldjkasc7lmla0n2q00gjkazgn919spfr-llm-ls-0.5.2/bin/",
-		},
+		model = "codellama:7b", -- The default model to use.
+		quit_map = "q", -- set keymap for close the response window
+		retry_map = "<c-r>", -- set keymap to re-send the current prompt
+		accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+		host = "localhost", -- The host running the Ollama service.
+		port = "11434", -- The port on which the Ollama service is listening.
+		display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
+		show_prompt = false, -- Shows the prompt submitted to Ollama.
+		show_model = false, -- Displays which model you are using at the beginning of your chat session.
+		no_auto_close = false, -- Never closes the window automatically.
+		hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+		init = function(options)
+			pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+		end,
+		-- Function to initialize Ollama
+		command = function(options)
+			local body = { model = options.model, stream = true }
+			return "curl --silent --no-buffer -X POST http://"
+				.. options.host
+				.. ":"
+				.. options.port
+				.. "/api/generate -d $body"
+		end,
+		-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+		-- This can also be a command string.
+		-- The executed command must return a JSON object with { response, context }
+		-- (context property is optional).
+		-- list_models = '<omitted lua function>', -- Retrieves a list of model names
+		debug = false, -- Prints errors and the command which is run.
 	},
 }
