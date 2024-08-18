bdd2bd9ba38ba360d97bfa0102c61ee8eca9a058
JustAlternate <loicw@justalternate.com>
2024-08-18 10:41:28 -0400

    feat(trying to setup ollama-copilot and changing icon)

--- home/rice/default.nix
+++ home/rice/default.nix
@@ -42,8 +42,8 @@
     enable = true;
 
     iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+      name = "Reversal";
+      package = pkgs.reversal-icon-theme;
     };
 
     theme = {
