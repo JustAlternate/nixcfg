c4096d19168fcd14fed8c803eff7fe8ee7a07c1c
JustAlternate <loicw@justalternate.com>
2024-08-18 10:41:45 -0400

    feat(ollama-copilot WIP)

--- home/nvim/ollama-copilot.nix
+++ home/nvim/ollama-copilot.nix
@@ -1,21 +1,29 @@
-{ lib, buildGoModule, fetchFromGitHub, ... }: {
-  pet = buildGoModule rec {
-    pname = "ollama-copilot";
-    version = "0.1";
+{ lib, buildGoModule, fetchFromGitHub, ... }:
+# self: super: {
+buildGoModule rec {
+  pname = "ollama-copilot";
+  version = "0.1";
 
-    src = fetchFromGitHub {
-      owner = "bernardo-bruning";
-      repo = "ollama-copilot";
-      rev = "v${version}";
-      hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
-    };
+  src = fetchFromGitHub {
+    owner = "bernardo-bruning";
+    repo = "ollama-copilot";
+    rev = "v${version}";
+    hash = "sha256-HjJcGU9Qwady1LKsOcSGkCQei1jID8l1o+mvqpFNjcI=";
+  };
 
-    vendorHash = "sha256-ciBIR+a1oaYH+H1PcC8cD8ncfJczk1IiJ8iYNM+R6aA=";
+  vendorHash = "sha256-6hCgv2/8UIRHw1kCe3nLkxF23zE/7t5RDwEjSzX3pBQ=";
 
-    meta = {
-      description = "Proxy that allows you to use ollama as a copilot like Github copilot ";
-      homepage = "https://github.com/bernardo-bruning/ollama-copilot";
-      maintainers = with lib.maintainers; [ bernardo-bruning ];
-    };
+  subPackage = [ "google/uuid" "ollama/ollama" ];
+
+  proxyVendor = true;
+
+  preBuild = ''
+    go mod vendor
+  '';
+
+  meta = {
+    description = "Proxy that allows you to use ollama as a copilot like Github copilot ";
+    homepage = "https://github.com/bernardo-bruning/ollama-copilot";
+    maintainers = with lib.maintainers; [ bernardo-bruning ];
   };
 }
