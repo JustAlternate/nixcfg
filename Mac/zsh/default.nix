671840410af59feddd9d77a8548719fc07a6c377
JustAlternateIDZ <loic.weber@iadvize.com>
2024-08-28 09:20:57 -0400

    feat(migrate mac nix files to this repo)

---
+++ Mac/zsh/default.nix
@@ -0,0 +1,47 @@
+{ pkgs, config, ... }: {
+  home = {
+    packages = with pkgs; [
+      fastfetch
+      zsh
+      zoxide
+      eza
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      ls = "eza --color=auto --icons=always";
+      cd = "z";
+      neofetch = "fastfetch";
+      lg = "lazygit";
+    };
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "dotenv" "vi-mode" ];
+      theme = "agnoster";
+    };
+
+    initExtra = ''
+      fastfetch
+      eval "$(zoxide init zsh)"
+    '';
+
+    sessionVariables = {
+      PATH = "/opt/homebrew/opt/openjdk/bin:/opt/homebrew/Cellar/python@3.11/bin:/usr/local/go/bin:${config.home.homeDirectory}/go/bin:$PATH";
+      GOPATH = "${config.home.homeDirectory}/go";
+      GOBIN = "${config.home.homeDirectory}/go/bin";
+    };
+  };
+}
