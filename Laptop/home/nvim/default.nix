7e6c207541d8c21b9f47ea7525131f4d1b9fdc35
JustAlternate <loicw@justalternate.com>
2024-09-02 04:23:19 -0400

    fix(nvim with pywal)

--- Laptop/home/nvim/default.nix
+++ Laptop/home/nvim/default.nix
@@ -1,22 +1,18 @@
 _:
-let
-  sharedConfig = import ../../../shared/nvim/default.nix;
-in
 {
   imports = [
     ../../../shared/nvim/default.nix
   ];
 
   # Append to the extra config config only for my laptop
-  programs.neovim.extraLuaConfig = sharedConfig.extraLuaConfig +
-    ''
-      local pywal16 = require('pywal16')
-      pywal16.setup()
-      local lualine = require('lualine')
-      lualine.setup {
-        options = {
-          theme = 'pywal16-nvim',
-        },
-      }
-    '';
+  programs.neovim.extraLuaConfig = ''
+    local pywal16 = require('pywal16')
+    pywal16.setup()
+    local lualine = require('lualine')
+    lualine.setup {
+      options = {
+        theme = 'pywal16-nvim',
+      },
+    }
+  '';
 }
