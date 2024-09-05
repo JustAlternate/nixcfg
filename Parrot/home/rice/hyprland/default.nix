b0f4a54b1be27d95774925f4503b857fe124cd9a
JustAlternate <cat /run/secrets/EMAIL>
2024-09-05 18:48:14 +0200

    feat(exit hyprland)

--- Parrot/home/rice/hyprland/default.nix
+++ Parrot/home/rice/hyprland/default.nix
@@ -112,8 +112,9 @@
       bind = $mainMod, C, exec, hyprctl keyword animation "fadeOut,0,0,default"; grimblast --freeze save area - | satty --filename - --fullscreen --output-filename ~/screenshots/$(date '+%Y%m%d-%H:%M:%S').png; hyprctl keyword animation "fadeOut,1,4,default"
       bind = $mainMod, B, exec, BEMOJI_PICKER_CMD="rofi -dmenu -theme ~/.config/rofi/bemoji.rasi" bemoji || killall rofi
 
+      bind = $mainMod, DELETE, exit,
+
       # Window management bindings
-      bind = $mainMod, M, exit,
       bind = $mainMod, Q, killactive,
 
       bind = $mainMod, F, togglefloating,
