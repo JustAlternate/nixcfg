1e1f5851afa472c5abb9566778833006b15f5803
JustAlternate <lololollololol890@gmail.com>
2024-09-12 11:50:59 +0200

    feat(ocaml lib)

--- Parrot/home/dev/Ocaml/default.nix
+++ Parrot/home/dev/Ocaml/default.nix
@@ -4,9 +4,14 @@
 {
   home = {
     packages = with pkgs; [
+      opam
       ocaml
+      ocamlPackages.findlib
+      ocamlPackages.lablgl
       ocamlPackages.ocaml-lsp
       ocamlPackages.ocamlformat
+      ocamlPackages.tsdl
+      ocamlPackages.tsdl-image
     ];
   };
 }
