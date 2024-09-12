ad6e905c2042954360e1641793678aadb355b576
JustAlternate <lololollololol890@gmail.com>
2024-09-12 12:36:28 +0200

    feat(pywal shared)

--- shared/pywal/default.nix
+++ shared/pywal/default.nix
@@ -15,8 +15,15 @@ in
     inputs.themecord.packages.x86_64-linux.default
   ];
 
-  home.file.".mozilla/native-messaging-hosts/pywalfox.json".text = lib.replaceStrings [ "<path>" ] [
-    "${pywalfox-wrapper}/bin/pywalfox-wrapper"
-  ]
-    (lib.readFile "${pkgs.pywalfox-native}/lib/python3.11/site-packages/pywalfox/assets/manifest.json");
+  home.file = {
+    "~/.config/wal/templates/cava_conf" = ./template/cava_conf;
+    "~/.config/wal/templates/color-rofi" = ./template/color-rofi;
+    "~/.config/wal/templates/colors-hypr" = ./template/colors-hypr;
+    "~/.config/wal/templates/colors-discord" = ./template/colors-discord.css;
+
+    ".mozilla/native-messaging-hosts/pywalfox.json".text = lib.replaceStrings [ "<path>" ] [
+      "${pywalfox-wrapper}/bin/pywalfox-wrapper"
+    ]
+      (lib.readFile "${pkgs.pywalfox-native}/lib/python3.11/site-packages/pywalfox/assets/manifest.json");
+  };
 }
