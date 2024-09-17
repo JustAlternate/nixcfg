22a80fa350063a5ab4d4f0d88eb66fb61dfee896
JustAlternate <cat /run/secrets/EMAIL>
2024-09-17 10:56:53 +0000

    WIP(gps)

--- Beaver/nginx/default.nix
+++ Beaver/nginx/default.nix
@@ -69,36 +69,24 @@
             ;
           };
         };
-        "gps.justalternate.fr" = {
-          enableACME = true;
-          forceSSL = true;
-          listen = [{ addr = "127.0.0.1"; port = 8443; ssl = true; }];
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:1212";
-            extraConfig = ''
-              ssl_protocols TLSv1.2 TLSv1.3;
-              ssl_ciphers 'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305';
-              ssl_session_cache shared:SSL:10m;
-              ssl_stapling on;
-              ssl_stapling_verify on;
-
-              ssl_ecdh_curve 'secp521r1:secp384r1';
-              ssl_prefer_server_ciphers on;
-              ssl_session_timeout 10m;
-              ssl_session_tickets off;
-
-              proxy_ssl_server_name on;
-              proxy_pass_header Authorization;
-
-              add_header Referrer-Policy same-origin always;
-              add_header X-Frame-Options DENY always;
-              add_header X-Content-Type-Options nosniff always;
-              add_header X-XSS-Protection "1; mode=block" always;
-              add_header X-Robots-Tag "noindex, nofollow" always;
-            '';
-          };
-
-        };
+        # "gps.justalternate.fr" = {
+        #   enableACME = true;
+        #   forceSSL = true;
+        #   listen = [{ addr = "127.0.0.1"; port = 8443; ssl = true; }];
+        #   locations."/" = {
+        #     proxyPass = "http://127.0.0.1:1212";
+        #     extraConfig = ''
+        #       proxy_ssl_server_name on;
+        #       proxy_pass_header Authorization;
+        #
+        #       add_header Referrer-Policy same-origin always;
+        #       add_header X-Frame-Options DENY always;
+        #       add_header X-Content-Type-Options nosniff always;
+        #       add_header X-XSS-Protection "1; mode=block" always;
+        #       add_header X-Robots-Tag "noindex, nofollow" always;
+        #     '';
+        #   };
+        # };
       };
     };
   };
