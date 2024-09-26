39068d3711a1f39b605e86d4aacf7c986d83be91
JustAlternate <cat /run/secrets/EMAIL>
2024-09-26 19:47:49 +0000

    feat(beaver hauk)

--- Beaver/nginx/default.nix
+++ Beaver/nginx/default.nix
@@ -69,24 +69,25 @@
             ;
           };
         };
-        # "gps.justalternate.fr" = {
-        #   enableACME = true;
-        #   forceSSL = true;
-        #   listen = [{ addr = "127.0.0.1"; port = 8443; ssl = true; }];
-        #   locations."/" = {
-        #     proxyPass = "http://127.0.0.1:1212";
-        #     extraConfig = ''
-        #       proxy_ssl_server_name on;
-        #       proxy_pass_header Authorization;
-        #
-        #       add_header Referrer-Policy same-origin always;
-        #       add_header X-Frame-Options DENY always;
-        #       add_header X-Content-Type-Options nosniff always;
-        #       add_header X-XSS-Protection "1; mode=block" always;
-        #       add_header X-Robots-Tag "noindex, nofollow" always;
-        #     '';
-        #   };
-        # };
+        "hauk.justalternate.fr" = {
+          enableACME = true;
+          forceSSL = true;
+          listen = [{ addr = "127.0.0.1"; port = 8443; ssl = true; }];
+
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:1212";
+            extraConfig = ''
+              proxy_ssl_server_name on;
+              proxy_pass_header Authorization;
+
+              add_header Referrer-Policy same-origin always;
+              add_header X-Frame-Options DENY always;
+              add_header X-Content-Type-Options nosniff always;
+              add_header X-XSS-Protection "1; mode=block" always;
+              add_header X-Robots-Tag "noindex, nofollow" always;
+            '';
+          };
+        };
       };
     };
   };
