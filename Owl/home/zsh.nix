6093db01e2f15a5401d0145f115599a9125264b2
JustAlternateIDZ <loic.weber@iadvize.com>
2024-11-14 11:20:48 +0100

    feat(Owl): zsh updated for swissknife and nvim config updated

--- Owl/home/zsh.nix
+++ Owl/home/zsh.nix
@@ -32,28 +32,21 @@
       enable = true;
       plugins = [
         "git"
-        "dotenv"
         "vi-mode"
       ];
       theme = "agnoster";
     };
 
     initExtra = ''
+      source ~/env-var/.env
       fastfetch
       eval "$(zoxide init zsh)"
     '';
 
     sessionVariables = {
-      PATH = "/opt/homebrew/opt/openjdk/bin:/opt/homebrew/Cellar/python@3.11/bin:/usr/local/go/bin:/opt/homebrew/bin:$PATH";
-      GOPATH = "/usr/local/go";
-      GOBIN = "/usr/go/bin";
-
-      # Usage of swissknife :
-      ARTIFACTORY_USERNAME = "justalternateidz";
-      # ARTIFACTORY_PASSWORD = "${config.sops.secrets.test-pass}"; #NOT WORKING
-      NOMAD_ADDR = "https://nomad.dev.iadvize.io";
-      GITHUB_USERNAME = "justalternateidz";
-      CORE_MYSQL_USER = "livechat";
+      PATH = "/opt/homebrew/opt/openjdk/bin:/opt/homebrew/Cellar/python@3.11/bin:/Users/loicweber/go/bin:/opt/homebrew/bin:$PATH";
+      GOPATH = "/Users/loicweber/go";
+      GOBIN = "/Users/loicweber/go/bin";
     };
   };
 }
