bc74ab3b4723c417268d8ed155a9e70248e8437b
JustAlternate <loicw@justalternate.fr>
2024-11-17 18:59:01 +0100

    WIP: FIXING NVIM PYWAL16

--- Gecko/configuration.nix
+++ Gecko/configuration.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ pkgs, lib, ... }:
 {
   nix = {
     settings = {
@@ -21,27 +16,36 @@
       options = "--delete-older-than 7d";
     };
   };
+  boot = {
 
-  # NixOS wants to enable GRUB by default
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+    # NixOS wants to enable GRUB by default
+    loader.grub.enable = false;
+    # Enables the generation of /boot/extlinux/extlinux.conf
+    loader.generic-extlinux-compatible.enable = true;
 
-  # !!! Set to specific linux kernel version
-  boot.kernelPackages = pkgs.linuxPackages;
+    # !!! Set to specific linux kernel version
+    kernelPackages = pkgs.linuxPackages;
 
-  # Disable ZFS on kernel 6
-  boot.supportedFilesystems = lib.mkForce [
-    "vfat"
-    "xfs"
-    "cifs"
-    "ntfs"
-  ];
+    # Disable ZFS on kernel 6
+    supportedFilesystems = lib.mkForce [
+      "vfat"
+      "xfs"
+      "cifs"
+      "ntfs"
+    ];
 
-  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
-  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
-  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
-  boot.kernelParams = [ "cma=256M" ];
+    # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
+    # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
+    # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
+    kernelParams = [ "cma=256M" ];
+
+    # forwarding
+    kernel.sysctl = {
+      "net.ipv4.conf.all.forwarding" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+      "net.ipv4.tcp_ecn" = true;
+    };
+  };
 
   # File systems configuration for using the installer's partition layout
   fileSystems = {
@@ -120,13 +124,6 @@
     #  address = "192.168.1.1";
     #  interface = "eth0";
     # };
-  };
-
-  # forwarding
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.forwarding" = true;
-    "net.ipv6.conf.all.forwarding" = true;
-    "net.ipv4.tcp_ecn" = true;
   };
 
   services.openssh.enable = true;
