0aa9d65d54e682afab9764a3808ab47f9b21f1ca
JustAlternate <loicw@justalternate.fr>
2024-11-23 11:36:17 +0100

    feat(dev): pygame WIP

--- shared/desktop/dev/python/default.nix
+++ shared/desktop/dev/python/default.nix
@@ -2,9 +2,14 @@
 {
   home = {
     packages = with pkgs; [
+      poetry
+      SDL2
+      SDL2_image
       (python3.withPackages (
         ps: with ps; [
           flask
+          pygame
+          pygame-sdl2
           matplotlib
           numpy
           opencv4
