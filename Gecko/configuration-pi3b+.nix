1ba198b8bed899ecfbcd6bc5fe86a7d130fd4c98
JustAlternate <loicw@justalternate.com>
2024-12-01 21:53:27 +0100

    feat(raspi config for 3b+)

--- Gecko/configuration-pi3b+.nix
+++ Gecko/configuration-pi3b+.nix
@@ -46,7 +46,6 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
