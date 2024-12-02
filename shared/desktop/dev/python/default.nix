9cba73e4f1723afe477a30595bc789917adbdb11
JustAlternate <loicw@justalternate.fr>
2024-12-02 09:01:02 +0100

    feat(dev): add python packages

--- shared/desktop/dev/python/default.nix
+++ shared/desktop/dev/python/default.nix
@@ -5,10 +5,13 @@
       poetry
       SDL2
       SDL2_image
+      SDL2_gfx
       (python3.withPackages (
         ps: with ps; [
           flask
+          hypothesis
           pygame
+          psutil
           pygame-sdl2
           psutil
           matplotlib
