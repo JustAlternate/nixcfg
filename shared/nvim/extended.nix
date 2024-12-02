b53fa878706def3fb2ac5775aa91aebbaeb1fca2
JustAlternate <loicw@justalternate.fr>
2024-12-02 15:16:01 +0100

    Merge

--- shared/nvim/extended.nix
+++ shared/nvim/extended.nix
@@ -1,11 +1,10 @@
-# File: nvim-module.nix
 { config, lib, ... }:
 {
   imports = [ ./default.nix ];
 
-  options.justalternate.neovim.pywal16.enable = lib.mkEnableOption "Enable pywal16 for neovim config";
+  options.custom.neovim.pywal16.enable = lib.mkEnableOption "Enable pywal16 for neovim config";
 
-  config = lib.mkIf config.options.custom.neovim.pywal16.enable {
+  config = lib.mkIf config.custom.neovim.pywal16.enable {
     programs.neovim.extraLuaConfig = ''
       local pywal16 = require('pywal16')
       pywal16.setup()
