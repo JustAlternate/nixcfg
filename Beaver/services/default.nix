850282de35f219eb88020cb8310936c15aba93fc
JustAlternate <loicw@justalternate.fr>
2025-03-05 21:35:03 +0000

    WIP(Beaver): traccar

--- Beaver/services/default.nix
+++ Beaver/services/default.nix
@@ -5,7 +5,6 @@ _: {
     ./vaultwarden
     ./openwebui # My personal llm ui backed by together ai for the inference API
     ./owncloud # selhosted google drivee
-    ./hauk # selfhosted google maps sharing location service
     ./mail
     # ./minecraft
     ./monitoring
