5253be09d64c4ce7e8b02b6f71fb078c1a9e0661
JustAlternate <loicw@justalternate.com>
2025-03-05 21:35:03 +0000

    WIP(Beaver): traccar

--- Beaver/services/traccar/default.nix
+++ Beaver/services/traccar/default.nix
@@ -5,12 +5,13 @@ _: {
       databasePassword = "$TRACCAR_PASSWORD";
       "web.enable" = "true";
       "web.port" = "5001";
-      "web.address" = "127.0.0.1";
       "protocol.port" = "5000";
       "server.timeout" = "900";
+      "server.statistics" = "";
     };
     environmentFile = "/run/secrets/TRACCAR";
   };
+
   services.nginx.virtualHosts."traccar.justalternate.fr" = {
     enableACME = true;
     forceSSL = true;
@@ -25,25 +26,16 @@ _: {
         ssl = true;
       }
     ];
+
     locations."/" = {
       proxyPass = "http://127.0.0.1:5001";
-      proxyWebsockets = true;
-      extraConfig = ''
-        proxy_ssl_server_name on;
-        proxy_pass_header Authorization;
-
-        add_header Referrer-Policy same-origin always;
-        add_header X-Frame-Options DENY always;
-        add_header X-Content-Type-Options nosniff always;
-        add_header X-XSS-Protection "1; mode=block" always;
-        add_header X-Robots-Tag "noindex, nofollow" always;
-
-        location ^~ /.well-known/acme-challenge/ {
-          # Serve ACME challenge over HTTP without redirect
-          default_type "text/plain";
-          root /var/lib/acme/acme-challenge;
-        }
-      '';
+      proxyWebsockets = true; # needed if you need to use WebSocket
+      extraConfig =
+        # required when the target is also TLS server with multiple hosts
+        "proxy_ssl_server_name on;"
+        +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;";
     };
   };
 }
