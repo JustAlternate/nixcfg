995926201b51c3aa0aef55030998fcf834039412
JustAlternate <loicw@justalternate.com>
2025-03-06 14:09:51 +0000

    chores(Beaver): remove geoloc services

--- Beaver/services/default.nix
+++ Beaver/services/default.nix
@@ -4,13 +4,12 @@ _: {
     ./planka # selfhosted Kanban
     ./vaultwarden
     ./openwebui # My personal llm ui backed by together ai for the inference API
-    ./owncloud # selhosted google drivee
+    ./owncloud # selhosted google drive
     ./mail
     # ./minecraft
     ./monitoring
     ./action-runner
     ./JustVPN
-    ./traccar
   ];
 
 }
