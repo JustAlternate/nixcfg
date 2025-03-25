ab602910ff74b4d25833b364ce30209e8104e198
JustAlternate <loicw@justalternate.com>
2025-03-25 10:06:53 +0000

    feat(All): multi modular zsh config

--- shared/zsh.nix
+++ shared/zsh.nix
@@ -1,5 +1,47 @@
 { pkgs, config, ... }:
 {
+  options.desktop = {
+    enable = pkgs.lib.mkOption {
+      type = pkgs.lib.types.bool;
+      default = false;
+    };
+  };
+
+  options.mac = {
+    enable = pkgs.lib.mkOption {
+      type = pkgs.lib.types.bool;
+      default = false;
+    };
+  };
+
+  config = pkgs.lib.mkIf config.desktop.enable {
+    programs.zsh = {
+      initExtraFirst = ''
+        cat ${config.home.homeDirectory}/.cache/wal/sequences
+      '';
+      initExtra = ''
+        source ~/env-var/.env
+      '';
+    };
+  };
+
+  config = pkgs.lib.mkIf config.mac.enable {
+    programs.zsh = {
+      initExtra = ''
+        source ~/env-var/.env
+      '';
+      shellAliases = {
+        db_connect = "~/github/system-toolbox/databases/./connect.sh";
+      };
+      sessionVariables = {
+        # Add path of homebrew installed app to my zshrc managed by nix
+        PATH = "/opt/homebrew/opt/php@7.4/bin:/opt/homebrew/opt/php@7.4/bin:/opt/homebrew/opt/openjdk/bin:/opt/homebrew/Cellar/python@3.11/bin:/Users/loicweber/go/bin:/opt/homebrew/bin:$PATH";
+        GOPATH = "/Users/loicweber/go";
+        GOBIN = "/Users/loicweber/go/bin";
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     zoxide
     tgpt
@@ -41,7 +83,6 @@
     initExtra = ''
       fastfetch
       eval "$(zoxide init zsh)"
-      source ~/env-var/.env
 
       bindkey '^J' history-incremental-search-backward
       bindkey '^K' history-incremental-search-forward
@@ -49,10 +90,6 @@
       bindkey -r '^S'
 
       bindkey '^O' autosuggest-accept
-    '';
-
-    initExtraFirst = ''
-      cat ${config.home.homeDirectory}/.cache/wal/sequences
     '';
   };
 }
