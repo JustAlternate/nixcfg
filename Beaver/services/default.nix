0b78e04664280d80017095994e8227b1dd11097b
JustAlternate <loicw@justalternate.com>
2025-03-30 14:20:02 +0000

    feat(Beaver): add pareto curve in nginx

--- Beaver/services/default.nix
+++ Beaver/services/default.nix
@@ -13,4 +13,33 @@ _: {
     ./gh-explorer
   ];
 
+  services.nginx.virtualHosts = {
+    "pareto.justalternate.com" = {
+      enableACME = true;
+      forceSSL = true;
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 80;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 8443;
+          ssl = true;
+        }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3939";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;"
+          +
+            # required when the server wants to use HTTP Authentication
+            "proxy_pass_header Authorization;";
+      };
+    };
+  };
+
 }
