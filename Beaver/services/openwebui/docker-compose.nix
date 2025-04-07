23853e1a3c0b3c6159f67cc62612e3f07f470175
JustAlternate <loicw@justalternate.fr>
2025-04-07 12:21:17 +0000

    chores(Beaver): bump open-webui

--- Beaver/services/openwebui/docker-compose.nix
+++ Beaver/services/openwebui/docker-compose.nix
@@ -14,7 +14,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."open-webui-openwebui" = {
-    image = "ghcr.io/open-webui/open-webui:0.6.0";
+    image = "ghcr.io/open-webui/open-webui:0.6.2";
     volumes = [
       "open-webui_open-webui:/app/backend/data:rw"
     ];
