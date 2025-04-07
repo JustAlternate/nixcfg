c26ce18640fa4e232e84e8500fa7fcbef12e43d6
JustAlternate <loicw@justalternate.com>
2025-04-07 11:02:27 +0200

    feat: remove ML machine

--- iac/infra/main.tf
+++ iac/infra/main.tf
@@ -11,7 +11,6 @@ provider "hcloud" {
   token = var.hetzner_token
 }
 
-
 resource "hcloud_network" "private_network" {
   name     = "private-network"
   ip_range = "10.0.0.0/16"
@@ -27,23 +26,4 @@ resource "hcloud_network_subnet" "default_private_network_subnet" {
 resource "hcloud_ssh_key" "my_ssh_key" {
   name       = "my-ssh-key"
   public_key = file("~/.ssh/id_ed25519.pub")
-}
-
-resource "hcloud_server" "GHExplorer" {
-  name        = "GHExplorer"
-  server_type = "cax11"
-  image       = "debian-12"
-  location    = "nbg1"
-  ssh_keys    = [hcloud_ssh_key.my_ssh_key.id]
-  network {
-    network_id = hcloud_network.private_network.id
-    # this will be the ip of the server in the private network
-    # the public ip won't be known until the server is provisioned
-    ip         = "10.0.1.3"
-  }
-}
-
-module "NixOS_install_preprod_server" {
-  source      = "./NixOS-install"
-  target_host = hcloud_server.GHExplorer.ipv4_address
 }
