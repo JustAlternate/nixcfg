ca122b5c6e35e97fcea4dbf29b800498bdfe5314
JustAlternate <loicw@justalternate.fr>
2025-06-14 12:42:33 +0000

    fix(Beaver): ci

--- Beaver/services/action-runner/default.nix
+++ Beaver/services/action-runner/default.nix
@@ -4,7 +4,6 @@
     "nixcfg" = {
       user = "root"; # TODO: Fix SOPS on beaver to be able to remove this for the CI.
       enable = true;
-      ephemeral = true;
       url = "https://github.com/JustAlternate/nixcfg";
       tokenFile = /run/secrets/ACTION_RUNNER/NIXCFG_TOKEN;
       extraPackages = with pkgs; [
