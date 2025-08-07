78f344cde14c3c3f7c8d24af7000da18d2732393
JustAlternate <loicw@justalternate.fr>
2025-08-07 20:09:04 +0000

    fix(Beaver): mail server in 25.05

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # home-manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
