e81d04e05cd4c18a8646bb21dc9061251cc6aa66
JustAlternate <loicw@justalternate.fr>
2025-09-28 12:19:53 +0000

    feat(Beaver): remove monitoring from openwebui

--- beaver/services/openwebui/default.nix
+++ beaver/services/openwebui/default.nix
@@ -1,20 +1,10 @@
 _: {
-  # imports = [ ./docker-compose.nix ];
-
   services = {
     nginx.virtualHosts."ai.justalternate.com" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:3040";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-      };
-    };
-    nginx.virtualHosts."monitor.justalternate.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:7878";
         proxyWebsockets = true; # needed if you need to use WebSocket
       };
     };
