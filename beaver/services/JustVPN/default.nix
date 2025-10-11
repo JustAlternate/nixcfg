78158f004cb3428f5851c7a08d3930f5d2aa0ae2
JustAlternate <loicw@justalternate.com>
2025-10-11 13:56:11 +0000

    fix(Beaver): wip

--- beaver/services/JustVPN/default.nix
+++ beaver/services/JustVPN/default.nix
@@ -1,22 +1,25 @@
 _: {
-  # imports = [ ./docker-compose.nix ];
-
   services.nginx.virtualHosts."vpn.justalternate.com" = {
     enableACME = true;
     forceSSL = true;
     locations = {
-      "/" = {
-        proxyPass = "http://127.0.0.1:3030/";
+      "/api/" = {
+        proxyPass = "http://127.0.0.1:3030";
+        recommendedProxySettings = true;
         proxyWebsockets = true;
         extraConfig = ''
-          proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header Connection "";
           proxy_read_timeout 500s;
           proxy_connect_timeout 500s;
           send_timeout 500s;
         '';
+      };
+      "/" = {
+        proxyPass = "http://127.0.0.1:3030/front/";
+        recommendedProxySettings = true;
       };
     };
   };
