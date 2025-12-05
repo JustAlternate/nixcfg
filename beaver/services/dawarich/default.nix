04725626a6b5dec68cd3c9330402613fb4ac9924
JustAlternate <loicw@justalternate.com>
2025-12-05 09:29:38 +0000

    feat(Beaver): dawarich wip

--- beaver/services/dawarich/default.nix
+++ beaver/services/dawarich/default.nix
@@ -1,12 +1,10 @@
 _: {
-  services = {
-    nginx.virtualHosts."geo.justalternate.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:3001";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-      };
-    };
+  imports = [
+    ./definition.nix
+  ];
+  services.dawarich = {
+    enable = true;
+    localDomain = "geo.justalternate.com";
+    webPort = 3001;
   };
 }
