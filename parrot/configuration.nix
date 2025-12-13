77f8c806e19d1d8d841520466dc41fec96ff6829
JustAlternate <loicw@justalternate.com>
2025-12-13 18:05:22 +0100

    feat(All): working hyprland module

--- parrot/configuration.nix
+++ parrot/configuration.nix
@@ -2,12 +2,15 @@
   config,
   inputs,
   pkgs,
+  lib,
   ...
 }:
 {
   # CONFIGURATION FOR A ASUS TUF Gaming A15 FA506ICB_FA506ICB
   imports = [
     ./hardware-configuration.nix
+
+    ./shared/desktop/hyprland
     ../shared/sops.nix
     ../shared/desktop/dev/docker/default.nix
     ../shared/optimise.nix
@@ -220,7 +223,7 @@
           "WLR_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1"
         ];
       };
-      extraConfig = ''
+      extraConfig = lib.mkMerge ''
         # Monitor settings
         monitor=eDP-1,1920x1080,0x1080,1
         monitor=HDMI-A-1,1920x1080,0x0,1
