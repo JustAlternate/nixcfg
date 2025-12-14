41c10f05222980f1f026a98ed20da1ca6abcc961
JustAlternate <loicw@justalternate.com>
2025-12-14 15:51:30 +0000

    feat(Beaver): add keycloak

---
+++ beaver/services/keycloak/default.nix
@@ -0,0 +1,23 @@
+_: {
+  services = {
+    keycloak = {
+      enable = true;
+      database.passwordFile = "/run/secrets/PASSWORD";
+      initialAdminPassword = "changeme";
+      settings = {
+        hostname = "https://auth.justalternate.com";
+        hostname-backchannel-dynamic = true; # Allow application hosted on the same local network as keycloak to enable internal communication
+        http-enabled = true; # https handled by our reverse proxy
+        http-port = 3127;
+        proxy-headers = "xforwarded";
+      };
+    };
+    nginx.virtualHosts."auth.justalternate.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3127";
+      };
+    };
+  };
+}
