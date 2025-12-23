0bd753ee07ebf4c994a807b9878dbd62f32be9d0
JustAlternate <loicw@justalternate.fr>
2025-12-23 11:40:14 +0100

    fix(Desktop): acticitywatcher

--- shared/desktop/activitywatch.nix
+++ shared/desktop/activitywatch.nix
@@ -1,15 +1,20 @@
 { pkgs, ... }:
 {
-  services = {
-    activitywatch = {
-      enable = true;
-      package = pkgs.aw-server-rust;
-      watchers = {
-        awatcher = {
-          package = pkgs.awatcher;
-          settings = {
-            poll_time = 3;
-          };
+  services.activitywatch = {
+    enable = true;
+    package = pkgs.aw-server-rust;
+    watchers = {
+      aw-watcher-afk = {
+        package = pkgs.activitywatch;
+        settings = {
+          timeout = 300;
+          poll_time = 2;
+        };
+      };
+      aw-watcher-window-wayland = {
+        package = pkgs.aw-watcher-window-wayland;
+        settings = {
+          poll_time = 1;
         };
       };
     };
