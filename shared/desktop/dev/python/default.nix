cbee58d26069ca49559703c83a06423ed6d48632
JustAlternate <loicw@justalternate.com>
2026-02-09 23:09:47 +0100

    fix(desktop): small fixes

--- shared/desktop/dev/python/default.nix
+++ shared/desktop/dev/python/default.nix
@@ -14,8 +14,6 @@
           flask
           keyboard
           # pyautogui
-          python313Packages.torchWithRocm
-          python313Packages.torchvision
           # sklearn-compat
           accelerate
           patool
--- shared/desktop/ghostty.nix
+++ shared/desktop/ghostty.nix
@@ -7,6 +7,8 @@ _: {
       macos-non-native-fullscreen = true;
       desktop-notifications = false;
       app-notifications = "no-clipboard-copy";
+      font-family = "Hack Nerd Font";
+      font-size = 20;
     };
     enableZshIntegration = true;
     clearDefaultKeybinds = false;
