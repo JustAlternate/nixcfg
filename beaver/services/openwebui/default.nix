a1b8d819cc82f9efc1e56e777fc42b454a5b84c9
JustAlternate <loicw@justalternate.com>
2026-02-10 22:32:38 +0100

    feat(Beaver): migrate open-webui to nix

--- beaver/services/openwebui/default.nix
+++ beaver/services/openwebui/default.nix
@@ -1,11 +1,31 @@
-_: {
+{ config, pkgs, ... }:
+{
   services = {
+    open-webui = {
+      enable = true;
+      package = pkgs.unstable.open-webui;
+      port = 3040;
+      host = "127.0.0.1";
+      openFirewall = false;
+      environmentFile = "/run/secrets/OPENWEBUI/ENV";
+    };
+
     nginx.virtualHosts."ai.justalternate.com" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:3040";
         proxyWebsockets = true;
+      };
+    };
+  };
+
+  systemd = {
+    services = {
+      open-webui = {
+        wants = [ "nginx.service" ];
+        after = [ "nginx.service" ];
+        bindsTo = [ "nginx.service" ];
       };
     };
   };
