70a884fc2e3f9fbe6a2bc5f944dbae189ef9f08b
JustAlternate <loicw@justalternate.com>
2026-02-10 23:34:31 +0100

    fix(desktop): ghostty

--- shared/desktop/ghostty.nix
+++ shared/desktop/ghostty.nix
@@ -2,24 +2,23 @@
 {
   programs.ghostty = {
     enable = true;
-    # package = null;
-    package = pkgs.ghostty;
+    package = if pkgs.stdenv.isDarwin then null else pkgs.ghostty;
     settings = {
-      # clipboard-read = "allow";
-      # clipboard-write = "allow";
-      # macos-non-native-fullscreen = true;
-      # desktop-notifications = false;
-      # app-notifications = "no-clipboard-copy";
-      # font-family = "Hack Nerd Font";
-      # macos-titlebar-proxy-icon = "hidden";
-      # title = "Terminal";
-      font-size = 25;
-      # quit-after-last-window-closed = true;
-      # quick-terminal-screen = "mouse";
-      # quick-terminal-autohide = true;
-      # window-decoration = "none";
-      # theme = "Catppuccin Frappe";
-      # keybind = "super+a=unbind";
+      clipboard-read = "allow";
+      clipboard-write = "allow";
+      macos-non-native-fullscreen = true;
+      desktop-notifications = false;
+      app-notifications = "no-clipboard-copy";
+      font-family = "Hack Nerd Font";
+      macos-titlebar-proxy-icon = "hidden";
+      title = "Terminal";
+      font-size = 18;
+      quit-after-last-window-closed = true;
+      quick-terminal-screen = "mouse";
+      quick-terminal-autohide = true;
+      window-decoration = "none";
+      theme = "Catppuccin Frappe";
+      keybind = "super+a=unbind";
     };
     enableZshIntegration = true;
     clearDefaultKeybinds = false;
