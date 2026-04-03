cabdd4552f0a728dc6d3f00db33e558e7663af63
JustAlternate <loicw@justalternate.com>
2026-04-03 19:20:52 +0200

    wip headscale

--- hosts/beaver/system/services/headscale/default.nix
+++ hosts/beaver/system/services/headscale/default.nix
@@ -6,7 +6,7 @@
     port = 9111;
     settings = {
       server_url = "https://headscale.justalternate.com";
-      dns.base_domain = "headscale.justalternate.com";
+      dns.base_domain = "dns.justalternate.com";
       dns.nameservers.global = [
         "1.1.1.1"
         "86.54.11.13"
